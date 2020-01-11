b1e36920be56b25488b7b9292334ba488d89973a
杜龙少 <sdvdxl@163.com>
2020-01-11 23:57:58 +0800

    add licnese header

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # dinghook
+
 钉钉 hook
+
+[钉钉机器人文档](https://ding-doc.dingtalk.com/doc#/serverapi2/qf2nxq)
--- message.go
+++ message.go
@@ -1,4 +1,18 @@
-//Package dinghook 详情参见 https://open-doc.dingtalk.com/docs/doc.htm?spm=a219a.7629140.0.0.karFPe&treeId=257&articleId=105735&docType=1
+// Copyright 2020 sdvdxl
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//Package dinghook 详情参见 https://ding-doc.dingtalk.com/doc#/serverapi2/qf2nxq
 package dinghook
 
 const (
